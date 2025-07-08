380b06ba51cb11d064c8c5c295ece9bba7031dc3
anhnt <tts.anhnt@vnpay.vn>
2025-07-08 17:18:47 +0700

    live

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,30 @@
-import { defineConfig } from "vite";
+import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tsconfigPaths from "vite-tsconfig-paths";
 import svgr from "vite-plugin-svgr";
 import path from "path";
 
 export default defineConfig({
-      base: "/trips-ticket/",
-
-  plugins: [react(), tsconfigPaths(), svgr()],
-  resolve: {
-    alias: {
-      "@styles": path.resolve(__dirname, "./src/assets/styles"),
+    base: "/trips-ticket/",
+    build: {
+        rollupOptions: {
+            output: {
+                format: "iife",
+            },
+        },
     },
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        // nếu muốn tự động thêm biến/mixin vào tất cả SCSS
-        // additionalData: `@import "@styles/utils/variables"; @import "@styles/utils/mixins";`
-      },
+    plugins: [react(), tsconfigPaths(), svgr()],
+    resolve: {
+        alias: {
+            "@styles": path.resolve(__dirname, "./src/assets/styles"),
+        },
     },
-  },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                // nếu muốn tự động thêm biến/mixin vào tất cả SCSS
+                // additionalData: `@import "@styles/utils/variables"; @import "@styles/utils/mixins";`
+            },
+        },
+    },
 });

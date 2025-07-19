f94357327e7fa03e0dde9cd7c72185b86a1a294b
anhnt <tts.anhnt@vnpay.vn>
2025-07-19 14:25:23 +0700

    fix

--- src/router/AppRoutes.tsx
+++ src/router/AppRoutes.tsx
@@ -1,5 +1,5 @@
-import React, { Suspense } from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import React, {Suspense} from "react";
+import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
 import MainLayout from "@/layouts/MainLayout";
 // import Home from "@/pages/Home/Home";
 import SearchPage from "@/pages/SearchPage/SearchPage";
@@ -7,18 +7,18 @@ import SearchPage from "@/pages/SearchPage/SearchPage";
 // import Loader from "@/components/Loader/Loader";
 
 const AppRoutes: React.FC = () => (
-  <BrowserRouter>
-    {/* <Suspense fallback={<Loader />}> */}
-      <Routes>
-        <Route element={<MainLayout />}>
-          <Route path="/" element={<SearchPage />} />
-          {/* <Route path="/search" element={<SearchPage />} /> */}
-          {/* <Route path="/history" element={<History />} /> */}
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Route>
-      </Routes>
-    {/* </Suspense> */}
-  </BrowserRouter>
+    <BrowserRouter basename="/trips-ticket">
+        {/* <Suspense fallback={<Loader />}> */}
+        <Routes>
+            <Route element={<MainLayout />}>
+                <Route path="/" element={<SearchPage />} />
+                {/* <Route path="/search" element={<SearchPage />} /> */}
+                {/* <Route path="/history" element={<History />} /> */}
+                <Route path="*" element={<Navigate to="/" replace />} />
+            </Route>
+        </Routes>
+        {/* </Suspense> */}
+    </BrowserRouter>
 );
 
 export default AppRoutes;
